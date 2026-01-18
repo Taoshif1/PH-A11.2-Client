a36a6a0073cfedc35096accfc53a4d29401f2ca9
Taoshif1 <taoshif2@gmail.com>
2026-01-18 14:29:28 +0600

    initial folder struct & packages added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center bg-amber-200">Blood Donation App Remastered 2.0</h1>
+
+    </>
+  )
+}
+
+export default App
